daf0dd329d9b391cc452e2b99f5ca4d2006d65fb
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-12-28 15:54:43 +0530

    add componet page - login, register, dashboard & setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Here i will make hero landing page */}
+        {/* <Route  path="" element={'/'}/> */}
+
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
