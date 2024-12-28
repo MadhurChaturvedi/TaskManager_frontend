0af02f257f99858025ddecce1e7f1010b2d891b7
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-12-28 16:45:20 +0530

    Header styling part ready

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         {/* Here i will make hero landing page */}
         {/* <Route  path="" element={'/'}/> */}
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import UseAnimations from "react-useanimations";
+import github from "react-useanimations/lib/github";
+import { BiLogIn } from "react-icons/bi";
+import { MdOutlineManageAccounts } from "react-icons/md";
+// infinity
+export default function Header() {
+    return (
+        <header className="flex justify-around items-center p-4 border-x-2">
+            <UseAnimations animation={github} size={45} />
+            <ul className="flex gap-7 items-center text-xl font-normal">
+                <Link
+                    to={"/"}
+                    className="text-black hover:text-blue-600 transition-all cursor-pointer flex items-center gap-1"
+                >
+                    <BiLogIn />
+                    LOGIN
+                </Link>
+                <Link
+                    to={"/login"}
+                    className="text-black hover:text-blue-600 transition-all cursor-pointer flex items-center gap-1"
+                >
+                    <MdOutlineManageAccounts />
+                    REGISTER
+                </Link>
+                {/* <Link to={"/register"} className='text-black hover:text-blue-600 transition-all cursor-pointer'>LOGOUT</Link> */}
+            </ul>
+        </header>
+    );
+}
