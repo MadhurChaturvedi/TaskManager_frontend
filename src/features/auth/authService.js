a64df740b110cc54bbf35d435dd403bb40f44410
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-12-30 20:14:54 +0530

    config env

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
 
-const API_URl = `http://localhost:8080/api/user/register`
-// console.log(import.meta.env.REACT_backend_URL);
-// Register user
-const register = async (userData) => {
-    const response = await axios.post(API_URl, userData)
 
+// Register user
+
+const register = async (userData) => {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/user/register`, userData)
+    
     if (response.data) {
         localStorage.setItem('user', JSON.stringify(response.data))
     }
     return response.data
 }
 
+
 const logout = async () => {
     localStorage.removeItem('user');
 }
 
 const login = async (userData) => {
-    const response = await axios.post('http://localhost:8080/api/user/login', userData)
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/user/login`, userData)
 
     if (response.data) {
         localStorage.setItem('user', JSON.stringify(response.data))
