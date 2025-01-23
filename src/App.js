08de60e36e5faa9b54c4cbb275711589db8307f4
Your Name <madhurchaturvedi2000@gmail.com>
2025-01-23 22:58:26 +0530

    fix the pageing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,15 +8,15 @@ import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Header />
       <Routes>
-
-        <Route path="/" element={<Dashboard />} />
+        {/* Redirect the root path "/" to "/login" */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/createForm" element={<TaskForm />} />
-
       </Routes>
       <ToastContainer autoClose={1000} />
     </BrowserRouter>
